584dd0a28cfd1f465610531c4fe402bbf62ed851
Jebakani <vjishwarya@gmail.com>
2021-08-02 11:02:29 +0530

    [JEBAKANI] Implementing Interfae segregation principle

---
+++ InterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterfaceSegregationPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Interface Segregation Principle");
+            OperationManagement operation = new OperationManagement();
+            List<EmployeeDetails> list = null;
+            for(int i=0;i<2;i++)
+            {
+                list = operation.ReadInput();
+            }
+            operation.GenerateEmployeeReport(list);
+        }
+    }
+}
